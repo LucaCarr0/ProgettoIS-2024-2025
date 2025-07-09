c083937a2158111aba8ae026038af2a5a4b4b6c2
Marco <81187102+MarcoCanonico@users.noreply.github.com>
2025-07-09 16:18:36 +0200

    Delete MySQL/DML/DML directory

--- MySQL/DML/DML/Utenti.sql
+++
@@ -1,7 +0,0 @@
-INSERT INTO Utenti (id, email, password, amministratore) VALUES
-(1, 'luca.carraturo8998@gmail.com',    'lucaadmin1.',    1),
-(2, 'bob@example.com',      'bobStandard123!',       0),
-(3, 'carla@example.com',    'carlaStandard123!',     0),
-(4, 'daniel@example.com',   'danielStandard123!',    0),
-(5, 'hater@example.com',    'haterStandard123!',     0),
-(6, 'alice@example.com',    'aliceStandard123!',     0);
