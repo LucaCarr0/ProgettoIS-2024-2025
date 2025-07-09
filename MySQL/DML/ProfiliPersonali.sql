b53cc7857dd9fbb828038fa228a9cb147fd46b01
Marco <81187102+MarcoCanonico@users.noreply.github.com>
2025-07-09 16:19:11 +0200

    Popolamento base v1
    Popolamento base di utenti,profili,raccolte,poesie,commenti

---
+++ MySQL/DML/ProfiliPersonali.sql
@@ -0,0 +1,7 @@
+INSERT INTO ProfiliPersonali (nome, cognome, immagineProfilo, biografia, nickname, dataNascita, utente) VALUES
+('Luca', 'Carraturo', NULL, 'Appassionato di poesia e scrittura creativa.', 'Lucol8', '2003-11-21', 1),
+('Bob', 'Smith', NULL, 'Amante della musica e della letteratura.', 'BobS', '1990-05-22', 2),
+('Carla', 'Bianchi', NULL, 'Scrittrice emergente e lettrice instancabile.', 'CarlaB', '1992-11-10', 3),
+('Daniel', 'Rossi', NULL, 'Critico letterario e appassionato di teatro.', 'DanielR', '1985-03-30', 4),
+('Hater', 'Nero', NULL, 'Sempre pronto a criticare, ma con passione.', 'HaterN', '1995-07-07', 5),
+('Alice', 'Verdi', NULL, 'Amante delle arti visive e della poesia moderna.', 'AliceV', '1988-12-01', 6);
