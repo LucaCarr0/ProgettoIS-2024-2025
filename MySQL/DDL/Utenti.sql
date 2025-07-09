18f296a6e796c01654adda4e20e3d3b472dcf3ea
Marco <81187102+MarcoCanonico@users.noreply.github.com>
2025-07-09 15:45:18 +0200

    popolamento utenti

---
+++ MySQL/DDL/Utenti.sql
@@ -0,0 +1,6 @@
+INSERT INTO Utenti (id, email, password, amministratore) VALUES
+(8000, 'alice@example.com',    'aliceStandard123!',     0),
+(8001, 'bob@example.com',      'bobStandard123!',       0),
+(8002, 'carla@example.com',    'carlaStandard123!',     0),
+(8003, 'daniel@example.com',   'danielStandard123!',    0),
+(8004, 'hater@example.com',    'haterStandard123!',     0);
