9a2f5ee72542ce5350f7a4a880fb88525869b9b9
Marco <81187102+MarcoCanonico@users.noreply.github.com>
2025-07-11 16:56:25 +0200

    commit MailService

---
+++ PoetUpMailService/src/email/EmailService.java
@@ -0,0 +1,45 @@
+package email;
+
+import jakarta.mail.*;
+import jakarta.mail.internet.*;
+
+import java.util.List;
+
+// Classe-Servizio che si occupa dell'invio effettivo delle email
+public class EmailService {
+    private final EmailConfig config;
+
+    public EmailService(EmailConfig config) {
+        this.config = config;
+    }
+
+    // Metodo per mandare una mail data una EmailMessage 
+    public void sendEmail(EmailMessage message) throws MessagingException {
+        // Crea una nuova sessione smtp (con autenticazione) per l'invio della mail 
+        // Per farlo viene fornito un Authenticator personalizzato che ridefinisce il metodo getPasswordAuthentication().
+        // Questo metodo viene chiamato automaticamente quando il server SMTP richiederà
+        // le credenziali. Se non lo avessi ridefinito la Session non saprebbe con che credenziali autenticarsi,
+        // e l'invio della mail fallirebbe (il metodo standard non ridefinito ritorna null smpre).
+        Session session = Session.getInstance(config.getProperties(), new Authenticator() {
+            @Override
+            protected PasswordAuthentication getPasswordAuthentication() {
+                return new PasswordAuthentication(config.getUsername(), config.getPassword());
+            }
+        });
+
+        // Dopo aver creato la session crea il messaggio email 
+        Message mimeMessage = new MimeMessage(session);
+        mimeMessage.setFrom(new InternetAddress(config.getUsername()));
+
+        List<String> recipients = message.getRecipients();
+        // Unisce i destinatari in una singola stringa separata da virgole
+        String recipientString = String.join(",", recipients);
+        mimeMessage.setRecipients(Message.RecipientType.TO, InternetAddress.parse(recipientString));
+
+        mimeMessage.setSubject(message.getSubject());
+        mimeMessage.setText(message.getBody());
+
+        // Invia il messaggio 
+        Transport.send(mimeMessage);
+    }
+}
