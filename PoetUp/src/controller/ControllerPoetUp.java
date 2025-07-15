ef37899cc0deef1db84cd8fe588d95dc0f830113
Luca <luca.carraturo8998@gmail.com>
2025-07-15 17:54:34 +0200

    commit doc

--- PoetUp/src/controller/ControllerPoetUp.java
+++ PoetUp/src/controller/ControllerPoetUp.java
@@ -7,11 +7,20 @@ import entity.EntityPoetUp;
 
 public class ControllerPoetUp {
 
+	 /**
+     * Registra un nuovo utente nel sistema con i dati forniti.
+     *
+     * @param nickname Il nickname scelto dall'utente.
+     * @param email L'indirizzo email dell'utente.
+     * @param pwd La password scelta dall'utente.
+     * @return Un messaggio che indica se la registrazione è andata a buon fine
+     *         o se l'utente è già esistente.
+     */
 	public static String registrazione(String nickname,String email,String pwd) {
 
 
 
-        // Salva il nuovo utente
+        
         Integer result = EntityPoetUp.registrazione(nickname,email,pwd);
 
         if (result==0) {
@@ -21,6 +30,14 @@ public class ControllerPoetUp {
 		}
 	}
 
+	/**
+     * Esegue l'autenticazione di un utente in base alle credenziali fornite.
+     *
+     * @param email L'email dell'utente.
+     * @param pwd La password associata all'email.
+     * @return Una stringa che indica se l'autenticazione è andata a buon fine
+     *         oppure è fallita.
+     */
 	public static String autenticazione(String email, String pwd) {
 		Integer result = EntityPoetUp.autenticazione(email,pwd);
 		if(result == 0) {
@@ -29,17 +46,33 @@ public class ControllerPoetUp {
 			return "Autenticazione fallita";
 		}
 	}
-	
+
+
+	/**
+     * Recupera l'elenco delle poesie da visualizzare nel feed principale.
+     *
+     * @return Una lista di oggetti {@link PoesiaDTO} contenente le poesie più recenti.
+     */
 	public static ArrayList<PoesiaDTO> visualizzaFeed() {
-		// TODO Auto-generated method stub
+		
 		ArrayList<PoesiaDTO> feed = EntityPoetUp.visualizzaFeed();
 		return feed;
 	}
 
+	/**
+     * Esegue una ricerca tra le poesie in base a un termine e un filtro.
+     *
+     * @param termineRicerca La parola chiave da cercare (es. parte del titolo o autore).
+     * @param filtro Il filtro da applicare alla ricerca (es. per data, popolarità, ecc.).
+     * @return Una lista di {@link PoesiaDTO} che soddisfano i criteri di ricerca.
+     */
 	public static ArrayList<PoesiaDTO> ricercaPoesie(String termineRicerca, String filtro) {
 	    return EntityPoetUp.ricercaPoesie(termineRicerca, filtro);
 	}
-	
+
+	/**
+     * Termina la sessione dell'utente attualmente loggato, modificando gli attributi di SessioneUtente.
+     */
 	public static void logout() {
 		EntityPoetUp.logout();
 	}
